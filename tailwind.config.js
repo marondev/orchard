4d8eed7ac3c875dfdd888a04d3af0a32cd88dcce
damedigital <tech@thedamedigital.com>
2023-05-01 10:18:54 +0800

    add default color and typography values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 const options = require("./config"); //options from config.js
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  darkMode: "class",
   theme: {
+    colors: {
+      body: '#0e1414',
+      primary: '#e6e7e7',
+      secondary: '#ffffff',
+      highlight: '#cf1430'
+    },
+    fontFamily: {
+      sans: ['Open Sans Light', ...defaultTheme.fontFamily.sans],
+      regular: ['Open Sans', 'sans-serif'],
+      bold: ['Open Sans Bold', 'sans-serif'],
+      condensed: ['Open Sans Condensed', 'sans-serif']
+    },
     extend: {},
   }
 };
