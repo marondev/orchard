9df47a581eb9539d6cb379b7ab74e656dcd14554
damedigital <tech@thedamedigital.com>
2023-05-01 13:25:00 +0800

    create modal and link clicking functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
+// This is to check the DOM is already loaded
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded Scripts");
 });
+
+function openModal(imageSrc) {
+  // Get the modal and modal image elements
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modal-image');
+
+  // Set the modal image source
+  modalImage.src = imageSrc;
+
+  // Show the modal
+  modal.classList.remove('hidden');
+  modal.classList.add('flex');
+}
+
+// Close the modal
+function closeModal() {
+  modal.classList.add('hidden');
+  modal.classList.remove('flex');
+}
+
+// Get all images with class .img
+const images = document.querySelectorAll('img.img');
+
+// Loop through images and add event listener
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    const imageSrc = image.getAttribute('data-image@2x');
+    openModal(imageSrc);
+  });
+});
+
+// Show data when clicking a link
+function gotoLink() {
+  console.log('Link has been clicked');
+  return false;
+}
